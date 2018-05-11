3af54b0e3a3d1af814c0a41313df4c12e2b5470b
mruthard@students.uni-mainz.de <Elki68946007>
2018-05-11 23:48:49 +0200

    A1_4 fertig

--- Projekt02_Wetter/Aufgabe1_4.py
+++ Projekt02_Wetter/Aufgabe1_4.py
@@ -24,6 +24,9 @@ class PlotWindow(qw.QDialog):
 
 		# Layout (wie Sie es bereits kennen)
         self.button = qw.QPushButton('Plot')
+        self.selectionList = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '17']
+
+        self.button.clicked.connect(self.getSelection)
         self.button.clicked.connect(self.plot)
         layout = qw.QVBoxLayout()
         layout.addWidget(self.toolbar)
@@ -32,37 +35,42 @@ class PlotWindow(qw.QDialog):
         self.setLayout(layout)
 
 	# Die Plot-Funktion kann nun wie vorher definiert werden:
+
+    def getSelection(self):
+        plt.cla()
+        sel = qw.QInputDialog.getItem(self, 'Selection Degree', 'Select degree for polynomial interpolation:', self.selectionList,
+                                      current=0, editable=False)
+        if sel[1]:
+            self.grad = int(sel[0])
+
     def plot(self):
         tabelle = np.loadtxt("wetterdaten2.txt", delimiter=';', skiprows=1, usecols=np.arange(15))
 
-        x = np.linspace(0,10, 10)
-        y = np.cos(x)*np.cos(2*x)+np.sin(x)
-
-        x1 = np.linspace(0, 10, 250)
-        y1 = np.cos(x1) * np.cos(2 * x1) + np.sin(x1)
-
-        xp = np.linspace(0, 10, 250)
-        yp= self.polynome(xp,x,y)
-
         yAchse = tabelle[:, 6]
-        spalte_rr = tabelle[:, 14]
-
-
         n = len(yAchse)
         xAchse = np.arange(n)
+        xAchseP = np.linspace(0, 18, 250)
+
         mit_x = np.sum(xAchse)/n
         mit_y = np.sum(yAchse)/n
-
-
         m = (n*mit_x*mit_y-np.sum(xAchse*yAchse))/(n*mit_x**2-np.sum(xAchse**2))
         b = mit_y - m*mit_x
-
         l_Reg = m*xAchse+b
+
+        grad = self.grad
+
+        z = np.polyfit(xAchse, yAchse, grad)
+        f = np.poly1d(z)
+        for i in range(len(xAchseP)):
+            zz = f(xAchseP)#, 'b+')
 
 
         # Zeichnen und Anzeige
-        self.axis.plot(xAchse, l_Reg,label="lineare Regression")
+        self.axis.plot(xAchse, l_Reg, label="lineare Regression")
         self.axis.plot(xAchse, yAchse, label="spalte_tx")
+        self.axis.plot(xAchseP, zz, label="polynomiell Regression, grad:" + str(grad))
+
+
         plt.legend()
 
         # Achtung: keine plt.show!
