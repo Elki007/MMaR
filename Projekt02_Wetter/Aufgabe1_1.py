ed13289940dcd1ab92e79a5a3f790f92c7cfee11
Gunnar Kohs <Gunnar.Kohs@gmx,de>
2018-05-13 19:58:37 +0200

    Aufgabe1_1 strukturiert und unnötiges gelöscht

--- Projekt02_Wetter/Aufgabe1_1.py
+++ Projekt02_Wetter/Aufgabe1_1.py
@@ -1,33 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#tabelle = np.genfromtxt("wetterdaten.txt", skip_header=3, missing_values=('nan'))
-#tabelle = np.loadtxt("wetterdaten.txt", skiprows=3)
-
+# Tabelle aus Datei lesen
 tabelle = np.loadtxt("wetterdaten2.txt", delimiter=';',skiprows=1, usecols=np.arange(15))
-#tabelle = tabelle[0:5,:]
 
-#print(tabelle)
-
-
+# 2 Spalten der Tabelle zuweisen
 spalte_tx = tabelle[:,6]
 spalte_rr = tabelle[:,14]
+
+# x-Achse von Zeilenanzahl abhängig machen
 xAchse = np.arange(len(spalte_rr))
 
 # Achsen
 fig, plot_tx = plt.subplots()
 plot_tx.plot(xAchse, spalte_tx, 'b-', label="Max Temp. 2m über dem Erdboden")
-plt.legend()
-# aus xlabel und xlim wird set_xlabel und set_xlim
-plot_tx.set_xlabel('Punkte')
 
-# Färben der y-Achse
-plot_tx.tick_params('y', colors='b')
+# Legende darstellen
+# ! was fehlt, um beide Labels in einer Legende darzustellen?
+plt.legend()
+
+# aus xlabel und xlim wird set_xlabel und set_xlim
+# ! Wozu ist diese Zeile?
+plot_tx.set_xlabel('Punkte')
 
 # zweite y-Achse (mit der gleichen x-Achse)
 plot_rr = plot_tx.twinx()
 plot_rr.plot(xAchse, spalte_rr, 'g-', label="Niederschlagsmenge")
 
+# Färben der y-Achsen
+plot_tx.tick_params('y', colors='b')
 plot_rr.tick_params('y', colors='g')
 
 # Zeichnen und Anzeige
@@ -35,4 +36,6 @@ plot_rr.tick_params('y', colors='g')
 #plt.plot(xAchse, spalte_rr, '--')
 plt.legend()
 plt.show()
+
+# ! Wozu diese Zeile?
 x= np.linspace(3, 9, 10)
