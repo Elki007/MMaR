e7c4a64f368d26de08711e55c68cf80d4a399c91
mruthard@students.uni-mainz.de <Elki68946007>
2019-01-22 12:39:53 +0100

    A2 - schlecht

--- Project2_05_05_Funktionenraeume/A2_a.py
+++ Project2_05_05_Funktionenraeume/A2_a.py
@@ -32,9 +32,17 @@ class DrawWidget(qw.QWidget):
         hbox = qw.QHBoxLayout()
         vbox = qw.QVBoxLayout()
 
-        b_new = qw.QPushButton('New path', self)
-        b_new.setToolTip('New path')
-        b_new.clicked.connect(self.on_click_new_path)
+        b_A2_c = qw.QPushButton('A2_c', self)
+        b_A2_c.setToolTip('A2_c')
+        b_A2_c.clicked.connect(self.pane.A2_main_c)
+
+        b_A2_b = qw.QPushButton('A2_b', self)
+        b_A2_b.setToolTip('A2_b')
+        b_A2_b.clicked.connect(self.pane.A2_main_b)
+
+        b_A2_b_np = qw.QPushButton('A2_b_np', self)
+        b_A2_b_np.setToolTip('A2_b')
+        b_A2_b_np.clicked.connect(self.pane.A2_main_b_np)
 
         b_clear = qw.QPushButton('Clear', self)
         b_clear.setToolTip('Clear the Graph')
@@ -52,7 +60,7 @@ class DrawWidget(qw.QWidget):
         self.cm_type.addItem("normal")
         self.cm_type.addItem("speed up")
         self.cm_type.addItem("slow down")
-        self.cm_type.activated.connect(self.on_click_type)"""
+        self.cm_type.activated.connect(self.on_click_type)
 
         cm_style = qw.QComboBox(self)
         cm_style.setToolTip('choose a style')
@@ -66,15 +74,17 @@ class DrawWidget(qw.QWidget):
 
         b_show_cv = qw.QCheckBox('Show CV', self)
         b_show_cv.clicked.connect(lambda: self.on_click_show_cv(b_show_cv.checkState()))
-        b_show_cv.setChecked(True)
+        b_show_cv.setChecked(True)"""
 
-        vbox.addWidget(b_new)
+        vbox.addWidget(b_A2_b)
+        vbox.addWidget(b_A2_b_np)
+        vbox.addWidget(b_A2_c)
         vbox.addWidget(b_clear)
         vbox.addWidget(b_clear_pane)
         # vbox.addWidget(b_plot)
         # vbox.addWidget(self.cm_type)
-        vbox.addWidget(cm_style)
-        vbox.addWidget(b_show_cv)
+        # vbox.addWidget(cm_style)
+        # vbox.addWidget(b_show_cv)
 
         vbox.addStretch(1)
 
@@ -160,6 +170,7 @@ class Pane(qw.QLabel):
         self.painter_cv.drawPath(self.current_path)
 
         self.current_cv = np.array([]).reshape(0, 2)
+        self.A2b = np.array([]).reshape(0, 2)
         self.showCV = True
 
         self.update()
@@ -180,7 +191,7 @@ class Pane(qw.QLabel):
         self.set_all_painters()
 
         self.update()
-        self.plot()
+        #self.plot()
 
     def fill_all_default(self):
         self.ebene_pane.fill(qg.QColor(0, 0, 0))
@@ -190,13 +201,13 @@ class Pane(qw.QLabel):
 
     def set_all_painters(self):
         self.painter_cv = qg.QPainter(self.ebene_cv)
-        self.painter_cv.setRenderHint(qg.QPainter.Antialiasing, True)
+        #self.painter_cv.setRenderHint(qg.QPainter.Antialiasing, True)
         self.painter_pane = qg.QPainter(self.ebene_pane)
-        self.painter_pane.setRenderHint(qg.QPainter.Antialiasing, True)
+        #self.painter_pane.setRenderHint(qg.QPainter.Antialiasing, True)
         self.painter_schlitten = qg.QPainter(self.ebene_schlitten)
         self.painter_total = qg.QPainter(self.ebene_total)
 
-        self.painter_pane.setPen(qg.QPen(qc.Qt.white, 3, qc.Qt.SolidLine))
+        self.painter_pane.setPen(qg.QPen(qc.Qt.white, 1, qc.Qt.SolidLine))
         self.painter_pane.drawLine(0, self.y_at, self.width(), self.y_at)
 
     def end_all_painters(self):
@@ -215,9 +226,14 @@ class Pane(qw.QLabel):
 
         self.cvs = []
         self.current_cv = np.array([]).reshape(0, 2)
+        self.A2b = np.array([]).reshape(0, 2)
         self.update()
 
     def undo(self):
+        self.paths = self.paths[:-1]
+        self.update()
+
+    def undo2(self):
         if len(self.current_cv) == 0:
             if len(self.cvs) != 0:
                 self.current_cv = self.cvs[-1]
@@ -240,16 +256,6 @@ class Pane(qw.QLabel):
 
     def update(self):
         """ draws control points (vertexes) and Paths """
-        """for i in range(self.k):
-            if len(self.paths) == 0:
-                return None
-            if len(self.paths[i]) != 0:
-                path = self.paths[i][-1][0]
-                thickness = self.paths[i][-1][1]
-                self.painter_pane.setPen(qg.QPen(self.color, thickness, qc.Qt.SolidLine))
-                self.painter_pane.drawPath(path)"""
-
-        """ draws control points and paths """
         self.ebene_cv.fill(qg.QColor(0, 0, 0, 0))
 
         if self.showCV:
@@ -274,31 +280,24 @@ class Pane(qw.QLabel):
                 self.painter_cv.setPen(qg.QPen(color, 6, qc.Qt.SolidLine))
                 self.painter_cv.drawPoint(self.current_cv[i][0], self.current_cv[i][1])"""
 
+        if self.A2b.size >1:
+            for x in range(self.A2b.size-1):
+                #self.painter_cv.drawPoint(x,self.A2b[x])
+                self.painter_cv.drawLine(x, self.A2b[x], x+1, self.A2b[x+1])
+
         self.ebene_total.fill(qg.QColor(0, 0, 0, 0))
         self.painter_total.drawPixmap(0, 0, self.ebene_pane)
         self.painter_total.drawPixmap(0, 0, self.ebene_cv)
         self.painter_total.drawPixmap(0, 0, self.ebene_schlitten)
         self.setPixmap(self.ebene_total)
 
-        self.get_all_values()
-        # print("drawing all")
-
-    def get_all_values(self):
-        img = qg.QImage(self.ebene_total)
-        #print(img.height() , img.width())
-        for x in range(img.width()):
-            for y in range(img.height()):
-                pixel = img.pixelColor(x,y)
-                if (pixel.red(),pixel.green(),pixel.blue()) != (0,0,0):
-                    print(pixel.red())
-
 
     def plot(self):
         self.painter_pane.end()
         self.ebene_pane.fill(qg.QColor(0, 0, 0))
         self.painter_pane = qg.QPainter(self.ebene_pane)
-        self.painter_pane.setRenderHint(qg.QPainter.Antialiasing, True)
-        self.painter_pane.setPen(qg.QPen(qc.Qt.white, 3, qc.Qt.SolidLine))
+        #self.painter_pane.setRenderHint(qg.QPainter.Antialiasing, True)
+        self.painter_pane.setPen(qg.QPen(qc.Qt.white, 1, qc.Qt.SolidLine))
         self.painter_pane.drawLine(0, self.y_at, self.width(), self.y_at)
 
         def p_1(t):
@@ -424,14 +423,6 @@ class Pane(qw.QLabel):
             if temp:
                 x, y, color = temp[0], temp[1], temp[2]
                 test(x, y, color)
-                """
-                path = qg.QPainterPath()
-
-                path.moveTo(x[0],y[0])
-                for j in range(len(x)):
-                    path.lineTo(x[j],y[j])
-
-                self.painter_pane.drawPath(path)"""
 
         # actual path, if it is not empty!
         if len(self.current_cv) != 0:
@@ -445,31 +436,114 @@ class Pane(qw.QLabel):
                 # x, y = x[:-5], y[:-5]
                 # print(x)
                 test(x, y, color)
-                """
-                path = qg.QPainterPath()
-
-                path.moveTo(x[0], y[0])
-                for j in range(len(x)):
-                    path.lineTo(x[j], y[j])
-                    if j+1 != len(x):
-                        if color[j] != color[j+1]:
-
-
-
-                self.painter_pane.drawPath(path)"""
 
         self.update()
 
+    def get_n(self):
+        return self.current_cv[:,0].size
 
-        # print(self.current_cv)
-        # p = bspline(self.test_cv, n=100)
-        # x, y = p.T
-        # print(x,y)
+    def get_t(self):
+        n = self.get_n()
+        return np.linspace(0, 2*np.pi, 2*n + 1)
 
-    """def lyambda_sin(self, m, n):
-        sum=0
-        for i in range(1,n):
-            sum += np.sin(m*)"""
+    def get_f_Ti(self, t):
+        x = self.current_cv[:, 0]
+        y = self.current_cv[:, 1] - self.y_at
+        x_vals = np.linspace(0, self.width(), self.width() + 1)
+        y_interp = np.interp(x_vals, x, y)
+
+        return y_interp[int(self.width() * t / (2 * np.pi))]
+
+
+    def lambda_m_sin(self, m):
+        n = self.get_n()
+        t = self.get_t()
+
+        subsum = 0
+        for i in range(n):
+            subsum += np.sin(m * t[i]) * self.get_f_Ti(t[i])
+        return 1/n * 2 * np.pi * subsum
+
+    def lambda_m_cos(self, m):
+        n = self.get_n()
+        t = self.get_t()
+
+        subsum = 0
+        for i in range(n):
+            subsum += np.cos(m * t[i]) * self.get_f_Ti(t[i])
+        return 1/n * 2 * np.pi * subsum
+
+
+    def A2_main_b(self):
+        self.paths.append(self.current_path)
+
+        if self.current_cv[:,0].size >1 :
+            if self.current_cv[-1][0] == self.width():
+                #print(self.lambda_m_sin(2))
+                path = qg.QPainterPath()
+
+                first = True
+                for t in self.get_t():
+                    y = 0
+
+                    for m in range(self.get_n()):
+                        y += self.lambda_m_sin(m) * np.sin(m * t) + self.lambda_m_cos(m) * np.cos(m * t)
+
+                    y += self.height()//2
+                    if first:
+                        path.moveTo(int(t / (2*np.pi)*self.width()), y)
+                        path.lineTo(int(t / (2*np.pi)*self.width()) + 0.1, y + 0.1)
+                    else:
+                        path.lineTo(int(t / (2 * np.pi) * self.width()), y)
+                    first = False
+                    #print(y)
+                self.paths.append(path)
+
+        self.update()
+
+    def A2_main_b_np(self):
+        self.paths.append(self.current_path)
+
+        x = self.current_cv[:, 0]
+        y = self.current_cv[:, 1] - self.y_at
+        x_vals = np.linspace(0, self.width(), self.width() + 1)
+        y_interp = np.interp(x_vals, x, y)
+        y_fft = np.fft.fft(y_interp)
+
+        path = qg.QPainterPath()
+        path.moveTo(x_vals[0],y_fft[0]+self.height()//2)
+        for i in range(len(y_fft)):
+            path.lineTo(x_vals[i], y_fft[i]+self.height()//2)
+
+        self.paths.append(path)
+        self.update()
+
+
+    def A2_main_c(self):
+        self.paths.append(self.current_path)
+
+        if self.current_cv[:,0].size >1 :
+            if self.current_cv[-1][0] == self.width():
+                #print(self.lambda_m_sin(2))
+                for m in range(5,7):
+                    path = qg.QPainterPath()
+                    first = True
+                    for t in self.get_t():
+                        y = self.lambda_m_sin(m) * np.sin(m * t) + self.height()/5
+                        if first:
+                            path.moveTo(int(t / (2*np.pi)*self.width()),y)
+                            path.lineTo(int(t / (2*np.pi)*self.width()) + 0.1, y + 0.1)
+                        else:
+                            path.lineTo(int(t / (2 * np.pi) * self.width()), y)
+                        first = False
+                    self.paths.append(path)
+        self.update()
+
+
+
+    #def plot_A2(self):
+
+
 
 
     def mousePressEvent(self, event):
@@ -477,16 +551,25 @@ class Pane(qw.QLabel):
         x = event.pos().x()
         y = event.pos().y()
         if self.current_path.isEmpty():
+            if x < 10:
+                x = 0
+            else:
+                return
             self.current_path.moveTo(x, y)
             self.current_path.lineTo(x + 0.1, y + 0.1)
             self.current_cv = np.r_[self.current_cv, [[x, y]]]
+            #self.fun_A2_a()
 
         if self.current_cv[-1][0] < x:
+            if x > self.width()-10:
+                x = self.width()
             self.current_path.lineTo(x, y)
             self.current_cv = np.r_[self.current_cv, [[x, y]]]
+            #self.fun_A2_a()
             # print(f"last:{self.current_cv[-1]}, current:{x}")
         # print(self.current_cv, self.current_cv[:, 0] )
 
+        #print(self.A2b)
         self.update()
         # self.plot()
 
