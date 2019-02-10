ea2a37fb79b743081cb290d0d77470ed1644c778
Max <maxruthard@yahoo.com>
2019-02-10 22:03:16 +0100

    TODO: some extra cases, intersection with another segment (when returned first n, but there is an intersection with n+1)

---
+++ Project2_06_Game/Classes/Explosion.py
@@ -0,0 +1,32 @@
+import random
+from Part import Part
+
+
+class Explosion:
+    def __init__(self, parent):
+        self.parent = parent
+        self.x_explosion = parent.x
+        self.y_explosion = parent.y
+
+        ###make smoke depend on current gun
+        self.amount = random.randint(10, 50)
+
+        self.timer = 100
+        self.destruct = False
+        self.smoke = []
+        self.parts = []
+
+        for i in range(self.amount):
+            self.smoke.append(Part(parent))
+
+    def paint(self):
+        if self.timer > 0:
+            for i in range(self.amount):
+                self.smoke[i].paint()
+            for i in range(len(self.parts) - 1, -1, -1):
+                self.parts[i].paint()
+
+            self.timer -= 1
+
+        else:
+            self.destruct = True
---
+++ Project2_06_Game/Classes/Part.py
@@ -0,0 +1,30 @@
+import PyQt5.QtWidgets as qw
+import PyQt5.QtCore as qc
+import PyQt5.QtGui as qg
+import random, math
+
+
+class Part:
+    def __init__(self, player):
+        self.parent = player
+        self.painter = player.painter
+        self.color = qg.QColor(255-random.randint(0,50), 255-random.randint(0,50), 255-random.randint(0,50))
+
+        self.gravity = player.g
+        self.part_vector = [0, 0]
+
+        self.x_part = self.parent.x
+        self.y_part = self.parent.y
+        self.weight = random.uniform(0.1, 0.3)
+        self.part_vector = [random.uniform(-1, 1), -1]
+
+        self.explosion_size = 2  # for particles
+
+    def paint(self):
+        self.x_part += self.part_vector[0]
+        self.y_part += self.part_vector[1]
+        self.part_vector[1] += (self.gravity * self.weight).y
+
+        self.painter.setPen(
+            qg.QPen(self.color, self.explosion_size, qc.Qt.SolidLine))
+        self.painter.drawPoint(self.x_part, self.y_part)
